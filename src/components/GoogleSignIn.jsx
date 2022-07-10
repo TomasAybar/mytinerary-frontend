5965040d303dd5181e811c4a392fb269814e7e5d
tomasAybar <aybar.t4@gmail.com>
2022-07-10 19:25:55 -0300

    cambios en la key

--- src/components/GoogleSignIn.jsx
+++ src/components/GoogleSignIn.jsx
@@ -39,7 +39,7 @@ export default function GoogleSignIn() {
     useEffect(() => {
         /* global google */
         google.accounts.id.initialize({
-            client_id: '165937703872-ssuvm1p099865ipv8d1e77sdq95c15ao.apps.googleusercontent.com',
+            client_id: '60710291903-kihnjrqu01bho3nmkhnfhu9p8595svno.apps.googleusercontent.com',
             callback: handleCallbackResponse
         });
 
